80e584f772e15f2003d3d05524a88bd6c7284be4
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-02-09 21:24:51 +0200

    add img and create downoad

--- src/components/download.vue
+++ src/components/download.vue
@@ -1,9 +1,43 @@
 <template>
-  <div>download</div>
+  <section class="container">
+    <div class="wrapper">
+      <div class="download">
+        <div class="download__content">
+          <h2 class="title">
+            Save time & money with exclusive offers from top stores
+          </h2>
+          <a href="#" class="btn primary">Download App</a>
+        </div>
+        <div class="download__app">
+          <img :src="require('../assets/image 14.png')" alt="sweava" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script>
-export default {};
-</script>
-
-<style></style>
+<style lang="scss" scoped>
+.download {
+  background: #fab8c5;
+  border-radius: 50px;
+  display: flex;
+  padding: 92px 115px;
+}
+.download__content {
+  flex-basis: 50%;
+  .primary {
+    padding: 25px 100px;
+  }
+  .title {
+    color: #5956e9;
+    font-size: 40px;
+    line-height: 52px;
+  }
+}
+.download__app {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+}
+</style>
