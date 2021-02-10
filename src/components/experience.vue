89bf8834b3ac27c21c35e3cadbc54620345cad58
yevheniimoskalenko <testjokerqwerty@gmail.com>
2021-02-10 12:14:41 +0200

    add element experiance

--- src/components/experience.vue
+++ src/components/experience.vue
@@ -2,6 +2,9 @@
   <section class="container">
     <div class="wrapper">
       <div class="experiance">
+        <div class="icon-oval-sec primary-c medium"></div>
+        <div class="icon-oval-s pink medium"></div>
+
         <div class="experiance__acc">
           <div class="experiance_content">
             <h2 class="title">Experience products in AR</h2>
@@ -11,6 +14,10 @@
             </p>
           </div>
           <div class="experiance__app">
+            <div class="icon-oval pink mega"></div>
+            <div class="icon-oval-t primary-c small"></div>
+            <div class="icon-oval-f pink medium"></div>
+
             <img
               :src="require('../assets/image 134.png')"
               class="borderImage"
@@ -29,6 +36,9 @@ export default {};
 
 <style lang="scss" scoped>
 .experiance {
+  position: relative;
+}
+.experiance {
   background: linear-gradient(
     72.77deg,
     #edeef6 11.72%,
@@ -43,9 +53,40 @@ export default {};
   align-self: flex-start;
 }
 .experiance__app {
+  position: relative;
   flex-basis: 50%;
   display: flex;
   justify-content: flex-end;
+  align-items: center;
+  img {
+    z-index: 3;
+  }
+}
+.icon-oval {
+  position: absolute;
+  top: 100px;
+  right: -44px;
+  z-index: 1;
+}
+.icon-oval-sec {
+  position: absolute;
+  top: -132px;
+  right: -50px;
+}
+.icon-oval-t {
+  position: absolute;
+  top: 100px;
+  right: -100px;
+}
+.icon-oval-f {
+  position: absolute;
+  top: 100px;
+  left: 0px;
+}
+.icon-oval-s {
+  position: absolute;
+  bottom: -50px;
+  left: 400px;
 }
 .experiance_content {
   flex-basis: 50%;
